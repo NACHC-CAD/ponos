e429a03260a394403269317b5e1ca9cc7d20fd0d
NACHC-CAD <greshje.nachc@gmail.com>
2023-06-15 10:25:06 -0400

    v1.0.003

---
+++ src/main/java/org/nachc/cad/tools/ponos/databricks/broadsea/ConnectDatabricksCdmToOhdsiOnBroadsea.java
@@ -0,0 +1,11 @@
+package org.nachc.cad.tools.ponos.databricks.broadsea;
+
+import org.nachc.tools.fhirtoomop.tools.databricks.OhdsiEnableExistingBroadseaOnDatabricksCdm;
+
+public class ConnectDatabricksCdmToOhdsiOnBroadsea {
+
+	public static void main(String[] args) {
+		OhdsiEnableExistingBroadseaOnDatabricksCdm.main(args);
+	}
+
+}
